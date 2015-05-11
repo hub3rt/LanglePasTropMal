5bf07910dd35b877054fce7d381f32cb1bcbac66
BaptisteLanusse <baptiste.lanusse@y-nov.com>
2015-05-11 12:02:19 +0200

    modif actionMailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.perform_deliveries = true
+
   config.action_mailer.delivery_method = :smtp
 
   # SMTP settings for gmail
