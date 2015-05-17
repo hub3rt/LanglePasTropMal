0f9ac9266eb2fceb04a9d9bd4fd4e46a1508887d
BaptisteLanusse <baptiste.lanusse@y-nov.com>
2015-05-17 13:32:18 +0200

    modif scrape.rb, ajout commentaires

--- scrape.rb
+++ scrape.rb
@@ -4,12 +4,22 @@ require 'open-uri'
 
 url = 'http://www.leboncoin.fr/voitures/offres/aquitaine/'
 data = Nokogiri::HTML(open(url))
-# offres = data.css('.lbc')
+offres = data.css('.lbc')
+
+# Récupérer la date et l'heure de la dernière annonce
+
+puts offres.first.css('.date').text
+
+# Vérifier si l'annonce la plus récente date d'aujourd'hui
+
+puts offres.first.css('.date').text.include? "Aujourd'hui"
+
+# Récupérer l'heure de l'annonce
 
 # puts offres.first.css('.date').text[/\d{2}:\d{2}/]
+
+# Afficher toutes les dates et heures des annonces de la page
 
 # offres.each do |offre|
 # 	puts offre.css('.date').text
 # end
-
-puts data.css('.search_category').text
