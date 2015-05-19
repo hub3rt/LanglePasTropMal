bd364289a54fdaed513003ad4d26790963e203f4
BaptisteLanusse <baptiste.lanusse@y-nov.com>
2015-05-19 15:59:27 +0200

    modif mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,8 +85,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   :user_name            => ENV['gmail_username'],
-   :password             => ENV['gmail_password'],
+   :user_name            => "babebenesse@gmail.com",
+   :password             => "jeplopdu42|",
    :authentication       => "plain",
   :enable_starttls_auto => true
   }
