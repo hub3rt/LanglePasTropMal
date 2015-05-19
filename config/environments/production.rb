ad9281f34f584429ef8ee84fbb740c4573a27e2e
BaptisteLanusse <baptiste.lanusse@y-nov.com>
2015-05-19 16:02:32 +0200

    modif mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,17 +77,11 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.delivery_method = :sendmail
+
   config.action_mailer.perform_deliveries = true
 
-  config.action_mailer.delivery_method = :smtp
-
-  # SMTP settings for gmail
-  config.action_mailer.smtp_settings = {
-   :address              => "smtp.gmail.com",
-   :port                 => 587,
-   :user_name            => "babebenesse@gmail.com",
-   :password             => "jeplopdu42|",
-   :authentication       => "plain",
-  :enable_starttls_auto => true
-  }
+  config.action_mailer.raise_delivery_errors = true
+  
+  config.action_mailer.default_options = {from: 'no-reply@example.com'}
 end
