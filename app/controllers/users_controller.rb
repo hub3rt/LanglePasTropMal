5ae8748f7043009825ac9c17c161dd17e03b3e93
BaptisteLanusse <baptiste.lanusse@y-nov.com>
2015-05-19 15:21:01 +0200

    modif action mailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        Mailer.welcome(@user).deliver
+        Mailer.welcome(@user).deliver_now
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
